7e6fe07c30e5a880c26b63112194015f6a5bb382
Ulovec.Lukas <uloveclukas@gmail.com>
2020-07-14 00:14:47 +0200

    14.07.2020-003

---
+++ 4kyu/SameStructureAs.js
@@ -0,0 +1,16 @@
+// Write a function/method (depending on the language) to return true/True 
+// when its argument is an array that has the same nesting structures 
+// and same corresponding length of nested arrays as the first array.
+
+Array.prototype.sameStructureAs = function (other) {
+    var first = JSON.stringify(this).replace(new RegExp("[0-9]", "g"), "X");
+    var second = JSON.stringify(other).toString().replace(new RegExp("[0-9]", "g"), "X");
+    if (first == second) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log([ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] ) == true);
+console.log([ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] ) == false);
